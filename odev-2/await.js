8168cc1dd1f904de382cee9f71ff33fd597e62f1
ibrahimkahramann <ibrahimk.bg06@gmail.com>
2023-07-04 12:46:11 +0300

    new work added

---
+++ odev-2/await.js
@@ -0,0 +1,36 @@
+const posts = [
+  { author: "User 1", content: "Lorem ipsum dolor" },
+  { author: "User 2", content: "Suspendisse ut lacus" },
+  { author: "User 3", content: "lviverra nec sapien sit amet" },
+];
+
+const listPosts = () => {
+  posts.map((post) => {
+    console.log(`User: ${post.author}\nContent: ${post.content}\n`);
+  });
+};
+
+const addContent = (newContent) => {
+    listPosts();
+  return new Promise((resolve, reject) => {
+    if (newContent) {
+        setTimeout(() => {
+            posts.push(newContent)
+            resolve();
+        },2000);
+    } else {
+      reject(10);
+    }
+  });
+};
+
+const showContents = async () => {
+    try {
+        await addContent({author:"user 10", content: "ut lacus nec sapien ipsum dolor"})
+        listPosts();
+    }catch(err){
+        console.log(`Bir hata olustu: ${err}`)
+    }
+}
+
+showContents();
